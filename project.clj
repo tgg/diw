1431bd618c168cb4f0dedac4392f079749121850
Thomas Girard <thomas.g.girard@free.fr>
2014-04-20 12:52:50 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojars.seancorfield/clj-soap "0.2.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojars.seancorfield/clj-soap "0.2.0"]]
   :main diw.core)
